befccc3fa335e534575ef2831aeb42afbd457dfd
divyashah9724330544 <divyashah9724330544@gmail.com>
2019-03-23 12:13:56 -0400

    newww

---
+++ HospitalNew/Migrations/20190322183015_feedback.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace HospitalNew.Migrations
+{
+    public partial class feedback : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Feedback",
+                columns: table => new
+                {
+                    FeedbackID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FeedbackQues = table.Column<string>(maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Feedback", x => x.FeedbackID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Feedback");
+        }
+    }
+}
---
+++ HospitalNew/Models/Feedback.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HospitalNew.Models
+{
+    public class Feedback
+    {
+        [Key]
+        public int FeedbackID { get; set; }
+
+        [Required, StringLength(255), Display(Name = "Question")]
+        public string FeedbackQues { get; set; }
+    }
+}
