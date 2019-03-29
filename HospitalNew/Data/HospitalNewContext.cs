ee14302f801d0fe98e7d6db09408a3a51d76430d
Olga Neyezhsal <neezhsal12@gmail.com>
2019-03-29 18:36:19 -0400

    add contex

--- HospitalNew/Data/HospitalNewContext.cs
+++ HospitalNew/Data/HospitalNewContext.cs
@@ -22,12 +22,56 @@ namespace HospitalNew.Models
         {
         }
 
+        public DbSet<Hospital> Hospitals { get; set; }
+        public DbSet<JobPosition> JobPositions { get; set; }
+        public DbSet<JobApplication> JobApplications { get; set; }
+        public DbSet<Volunteer> Volunteers { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        //public DbSet<BookingApp> BookingApps { get; set; }
         public DbSet<Feedback> Feedback { get; set; }
+
+
+
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            //following the diagram on the notebook (picture included in assets folder)
 
-            modelBuilder.Entity<Feedback>().ToTable("Feedback");
+            modelBuilder.Entity<JobApplication>()
+                .HasOne(j => j.jobposition)
+                .WithMany(ja => ja.jobapplications)
+                .HasForeignKey(j => j.JobPositionID);
+
+            //QUESTION FOR GROUP: IS SOMEONE DOING DEPARTMENTS? ans:No
+
+            //eash job position references one hospital, one hospital has many job position
+
+            modelBuilder.Entity<JobPosition>()
+                .HasOne(h => h.Hospital)
+                .WithMany(j => j.JobPositions)
+                .HasForeignKey(h => h.HospitalID);
+
+            //each volunteer references one hospital, one hospital has many volunteer
+
+            modelBuilder.Entity<Volunteer>() //WE ARE TALKING ABOUT VOLUNTEERS
+                .HasOne(v => v.Hospital) //WE ARE TALKING ABOUT HOSPITALS
+                .WithMany(h => h.Volunteers)
+                .HasForeignKey(v => v.HospitalID);
+
+            //each job position has one department, each department has many job
+            modelBuilder.Entity<JobPosition>()
+                .HasOne(d => d.Department)
+                .WithMany(j => j.JobPositions)
+                .HasForeignKey(d => d.DepartmentID);
+
+
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Feedback>().ToTable("Feedback");
+            modelBuilder.Entity<Hospital>().ToTable("Hospitals");
+            modelBuilder.Entity<JobPosition>().ToTable("JobPositions");
+            modelBuilder.Entity<JobApplication>().ToTable("JobApplications");
+            modelBuilder.Entity<Volunteer>().ToTable("Volunteers");
+            modelBuilder.Entity<Department>().ToTable("Departments");
         }
 
     }
